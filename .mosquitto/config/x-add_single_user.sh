e99ece9e6c13621ebc0ce062c17e656a31f3ff6b
fkrenn12 <fkrenn@nanet.at>
2025-12-22 10:30:42 +0100

    Add username argument to mosquitto_passwd command
    Previously, the username was not passed to the mosquitto_passwd command, requiring manual input. This change ensures the username is supplied as an argument, streamlining the process and improving efficiency.

--- .mosquitto/config/x-add_single_user.sh
+++ .mosquitto/config/x-add_single_user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo -n "Enter Username: "
 read Username
-docker exec -it mosquitto mosquitto_passwd /mosquitto/config/passwd
+docker exec -it mosquitto mosquitto_passwd /mosquitto/config/passwd $Username
 docker exec -it mosquitto sh -c 'kill -HUP $(cat /mosquitto/config/pid)'
 
