b3a96aa7360ef71c42bc038fa32a72ec88022031
fkrenn12 <fkrenn@nanet.at>
2025-12-22 10:24:39 +0100

    Refactor Mosquitto scripts and improve file permission handling
    Removed unnecessary username parameter in user management scripts and standardized command formatting. Updated file permissions and added clarity on self-signed certificate usage in Docker Compose for better configuration manageability.

--- .mosquitto/config/x-add_single_user.sh
+++ .mosquitto/config/x-add_single_user.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo -n "Enter Username: "
 read Username
-docker exec -it mosquitto mosquitto_passwd /mosquitto/config/passwd $Username
+docker exec -it mosquitto mosquitto_passwd /mosquitto/config/passwd
 docker exec -it mosquitto sh -c 'kill -HUP $(cat /mosquitto/config/pid)'
-# mosquitto_passwd /mosquitto/config/passwd $Username
-# kill -HUP $(cat /mosquitto/config/pid)
+
--- .mosquitto/config/x-delete_single_user.sh
+++ .mosquitto/config/x-delete_single_user.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo -n "Enter Username: "
 read Username
-docker exec -it mosquitto "sed -i '/^$Username/d' /mosquitto/config/passwd"
+docker exec -it mosquitto sed -i "/^$Username/d" /mosquitto/config/passwd
 docker exec -it mosquitto sh -c 'kill -HUP $(cat /mosquitto/config/pid)'
-
